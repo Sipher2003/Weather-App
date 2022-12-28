4dff991f3a91784ae6ab334fc56b47f4c3a77563
Sipher <kaustubh.bhosale30@gmail.com>
2022-12-28 12:57:37 +0530

    updates

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const options = {
 
 const getweather = (city) => {
     cityname.innerHTML = city;   // cityname is the h1 tag
+    cityname2.innerHTML = city;   // cityname is the h1 tag
+    cityname3.innerHTML = city;   // cityname is the h1 tag
+    cityname4.innerHTML = city;   // cityname is the h1 tag
+    cityname5.innerHTML = city;   // cityname is the h1 tag
+    cityname6.innerHTML = city;   // cityname is the h1 tag
+    cityname7.innerHTML = city;   // cityname is the h1 tag
+
   fetch(
     "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city="+city ,  // city is the input field
     options
@@ -17,12 +24,18 @@ const getweather = (city) => {
       console.log(response);
 
       // cloud_pct.innerHTML = response.cloud_pct
-    //   temp.innerHTML = response.temp;
+      temp.innerHTML = response.temp;
+      temp2.innerHTML = response.temp;
         feels_like.innerHTML = response.feels_like
-      //   humidity.innerHTML = response.humidity
+        feels_like2.innerHTML = response.feels_like
+        humidity.innerHTML = response.humidity
+        humidity2.innerHTML = response.humidity
       min_temp.innerHTML = response.min_temp;
+      min_temp2.innerHTML = response.min_temp;
       max_temp.innerHTML = response.max_temp;
-      //   wind_speed.innerHTML = response.wind_speed
+      max_temp2.innerHTML = response.max_temp;
+        wind_speed.innerHTML = response.wind_speed
+        wind_speed2.innerHTML = response.wind_speed
       //   wind_degrees.innerHTML = response.wind_degrees
       //   sunrise.innerHTML = response.sunrise
       //   sunset.innerHTML = response.sunset
